c67aa124efc94408b6cd41de65fcebd53f8668c6
Vera <mokeeva.vk@phystech.edu>
2023-04-24 19:37:03 +0300

    using middlewar &  applyMiddleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,15 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+
+const stringMiddleware = () => (next) => (action) => {
+    if(typeof action === 'string') {
+        return next({
+            type: action 
+        })
+    }
+    return next(action)
+};
 
 const enhancer = (createStore) => (...args) => {
     const store = createStore(...args);
@@ -17,11 +26,13 @@ const enhancer = (createStore) => (...args) => {
     return store;
 }
 
-const store = createStore( combineReducers(
-                    {heroes, filters}), 
-                    compose(
-                        enhancer,
-                        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                    ));
+const store = createStore( 
+                    combineReducers({heroes, filters}), 
+                    applyMiddleware(stringMiddleware)
+                    // compose(
+                    //     enhancer,
+                    //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+                    // )
+                    );
 
 export default store;
