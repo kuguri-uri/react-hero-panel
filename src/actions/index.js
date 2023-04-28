7897ae37ef49895d61e2aacac41c8a52e0da3eb5
Vera <mokeeva.vk@phystech.edu>
2023-04-28 17:53:55 +0300

    with create reducer from toolkit

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,18 +15,22 @@ export const fetchHeroes =  (request) => (dispatch) => {
 
 export const heroesFetching = createAction('HEROES_FETCHING');
 
-export const heroesFetched = (heroes) => {
-    return {
-        type: 'HEROES_FETCHED',
-        payload: heroes
-    }
-}
+// export const heroesFetched = (heroes) => {
+//     return {
+//         type: 'HEROES_FETCHED',
+//         payload: heroes
+//     }
+// }
 
-export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
+export const heroesFetched = createAction('HEROES_FETCHED');
+
+// export const heroesFetchingError = () => {
+//     return {
+//         type: 'HEROES_FETCHING_ERROR'
+//     }
+// }
+
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR')
 
 export const fetchFilters = (request) => (dispatch) => {
     dispatch(filtersFetching());
@@ -71,16 +75,20 @@ export const activeFilterChanged = (filter) => {
 //     }, 1000)
 // }
 
-export const heroDeleted = (id) => {
-    return {
-        type: 'DELETE',
-        payload: id
-    }
-}
+// export const heroDeleted = (id) => {
+//     return {
+//         type: 'DELETE',
+//         payload: id
+//     }
+// }
 
-export const heroCreated = (hero) => {
-    return {
-        type: 'HERO_CREATED',
-        payload: hero
-    }
-}
+export const heroDeleted = createAction('DELETE');
+
+// export const heroCreated = (hero) => {
+//     return {
+//         type: 'HERO_CREATED',
+//         payload: hero
+//     }
+// }
+
+export const heroCreated = createAction('HERO_CREATED');
