a85714937edb4948b94532a39d4152f73767a2d2
Vera <mokeeva.vk@phystech.edu>
2023-05-02 13:40:40 +0300

    fetchHeroes with createAsyncThunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,4 @@
-import { heroesFetching, heroesFetched, heroesFetchingError } from "../components/heroesList/heroesSlice";
 import { filtersFetched, filtersFetching, filtersFetchingError } from "../components/heroesFilters/filtersSlice";
-
-export const fetchHeroes =  (request) => (dispatch) => {
-    dispatch(heroesFetching());
-    request("http://localhost:3001/heroes")
-        .then(data => dispatch(heroesFetched(data)))
-        .catch(() => dispatch(heroesFetchingError()))
-}
 
 export const fetchFilters = (request) => (dispatch) => {
     dispatch(filtersFetching());
@@ -15,38 +7,3 @@ export const fetchFilters = (request) => (dispatch) => {
         .catch(() => dispatch(filtersFetchingError()))
 
 }
-
-// export const filtersFetching = () => {
-//     return {
-//         type: 'FILTERS_FETCHING'
-//     }
-// }
-
-// export const filtersFetched = (filters) => {
-//     return {
-//         type: 'FILTERS_FETCHED',
-//         payload: filters
-//     }
-// }
-
-// export const filtersFetchingError = () => {
-//     return {
-//         type: 'FILTERS_FETCHING_ERROR'
-//     }
-// }
-
-// export const activeFilterChanged = (filter) => {
-//     return {
-//         type: 'ACTIVE_FILTER_CHANGED',
-//         payload: filter
-//     }
-// }
-
-// export const activeFilterChanged = (filter) => (dispatch) => {
-//     setTimeout(() => {
-//         dispatch({
-//             type: 'ACTIVE_FILTER_CHANGED',
-//             payload: filter
-//         })
-//     }, 1000)
-// }
